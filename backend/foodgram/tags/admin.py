3a1ff5fee28b7f6199c84cb8ee9b61d277b4589c
Герман Леонтьев <gerleonsto@gmail.com>
2023-05-15 23:39:35 +0300

    add model Tags and admin zone

--- backend/foodgram/tags/admin.py
+++ backend/foodgram/tags/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tags
+
+
+@admin.register(Tags)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'color',
+        'slug',
+    )
+    search_fields = ('name',)
+    list_filter = ('color',)
+    list_editable = ('color',)
+    empty_value_display = '-пусто-'
+    prepopulated_fields = {'slug': ('name',)}
--- backend/foodgram/tags/models.py
+++ backend/foodgram/tags/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Tags(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200,
+        help_text='введите название тега',
+    )
+    color = models.CharField(
+        verbose_name='Цвет',
+        max_length=16,
+        help_text='введите цвет тега (HEX)',
+        blank=True,
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        verbose_name='адрес',
+        help_text='введите уникальный слаг',
+        blank=True,
+    )
+
+    def __str__(self) -> str:
+        return self.name
