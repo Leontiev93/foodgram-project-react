d2158b956e9bbfdea12cad3c0e96c6940980e253
Герман Леонтьев <gerleonsto@gmail.com>
2023-05-17 23:57:57 +0300

    update model delete apps ingredients

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,24 +1,39 @@
 from django.contrib import admin
 
-from .models import Recipes, IngredientsToRecipes
+from .models import Recipes, IngredientsToRecipes, Ingredient
 
 
 @admin.register(Recipes)
 class RecipesAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        # 'tags',
-        # 'ingredients',
+    fields = [
+        'tags',
         'is_favorited',
         'is_in_shopping_cart',
         'name',
         'cooking_time',
+        'author',
+#        'ingredients'
+    ]
+    list_display = (
+        'pk',
+        '_tags',
+#        '_ingredients',
+        'is_favorited',
+        'is_in_shopping_cart',
         'name',
+        'cooking_time',
     )
     search_fields = ('name',)
     list_filter = ('is_favorited',)
     list_editable = ('is_favorited', 'is_in_shopping_cart')
     empty_value_display = '-пусто-'
+
+    def _tags(self, obj):
+        return ', '.join([tag.name for tag in obj.tags.all()])
+
+    def _ingredients(self, obj):
+        return '\n'.join([ingredient.name for ingredient in obj.ingredients.all()])
+
 
 
 @admin.register(IngredientsToRecipes)
@@ -31,4 +46,23 @@ class IngredientsToRecipesAdmin(admin.ModelAdmin):
     )
     search_fields = ('recipes',)
     list_filter = ('recipes',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Ingredient)
+class AdminIngredient(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'measurement_unit',
+    )
+    search_fields = (
+        'name',
+        'measurement_unit',
+    )
+    list_filter = (
+        'name',
+        'measurement_unit',
+    )
+    list_editable = ('measurement_unit',)
     empty_value_display = '-пусто-'
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
-from ingredients.models import Ingredient
 from tags.models import Tags
 from users.models import User
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        verbose_name='Название продукта',
+        max_length=200,
+    )
+    measurement_unit = models.CharField(
+        verbose_name='Единица измерения',
+        max_length=200,
+    )
+
+    def __str__(self) -> str:
+        return f'{self.name} измеряется в {self.measurement_unit}'
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Игридиенты'
 
 
 class Recipes(models.Model):
@@ -14,6 +32,11 @@ class Recipes(models.Model):
         verbose_name='автор рецепта',
         help_text='Автор рецепта',
     )
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+        help_text='добавьте название блюда',
+    )
     tags = models.ManyToManyField(
         Tags,
         null=True,
@@ -23,11 +46,17 @@ class Recipes(models.Model):
     )
     ingredients = models.ManyToManyField(
         Ingredient,
+        related_name='recipes',
         through='IngredientsToRecipes'
     )
-    is_favorited = models.BooleanField()
-    is_in_shopping_cart = models.BooleanField()
-    name = models.CharField(max_length=200)
+    is_favorited = models.BooleanField(
+        verbose_name='Избранное',
+        help_text='добавить в избраное',
+    )
+    is_in_shopping_cart = models.BooleanField(
+        verbose_name='Корзина',
+        help_text='добавить в корзину покупок',
+    )
     image = models.ImageField(upload_to='media/photos/%Y%M%d/')
     text = models.TextField(
         verbose_name='описание',
@@ -72,4 +101,4 @@ class IngredientsToRecipes(models.Model):
 
     class Meta:
         verbose_name = 'Ингридиенты к рецептам'
-        verbose_name_plural = 'Ингридиенты'
+        verbose_name_plural = 'Ингридиенты к рецептам'
