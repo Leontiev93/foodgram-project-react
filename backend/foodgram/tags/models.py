0104ab2e0f6104b8608a0041d734d95e70962650
Герман Леонтьев <gerleonsto@gmail.com>
2023-06-08 12:03:13 +0300

    Corrected remarks tags

--- backend/foodgram/tags/models.py
+++ backend/foodgram/tags/models.py
@@ -1,24 +1,31 @@
 from django.db import models
+from django.contrib.auth.validators import UnicodeUsernameValidator
+
+from tags.validators import HexValidator
+
+LENGTH = 200
 
 
 class Tags(models.Model):
     name = models.CharField(
         verbose_name='Название',
-        max_length=200,
+        max_length=LENGTH,
         help_text='введите название тега',
+        validators=(UnicodeUsernameValidator(),
+                    )
     )
     color = models.CharField(
         verbose_name='Цвет',
         max_length=16,
         help_text='введите цвет тега (HEX)',
-        blank=True,
+        validators=(HexValidator(),
+                    )
     )
     slug = models.SlugField(
-        max_length=200,
+        max_length=LENGTH,
         unique=True,
         verbose_name='адрес',
         help_text='введите уникальный слаг',
-        blank=True,
     )
 
     def __str__(self) -> str:
---
+++ backend/foodgram/tags/validators.py
@@ -0,0 +1,13 @@
+from django.core import validators
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import gettext_lazy as _
+
+
+@deconstructible
+class HexValidator(validators.RegexValidator):
+    regex = r"^#([0-9a-fA-F]{3}){1,2}$"
+    message = _(
+        "Начало должна быть с # далее любое"
+        "целое число от 0 до 9 и любая буква от A до F."
+    )
+    flags = 0
