9c52e03a174a2ce56fdf23bb4d3b2397a82ba45f
Герман Леонтьев <gerleonsto@gmail.com>
2023-06-12 17:05:14 +0300

    review3

--- backend/foodgram/api/v1/views.py
+++ backend/foodgram/api/v1/views.py
@@ -1,5 +1,4 @@
-from rest_framework.pagination import (
-    LimitOffsetPagination)
+from rest_framework.pagination import LimitOffsetPagination
 from django.db.models import Sum
 from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
